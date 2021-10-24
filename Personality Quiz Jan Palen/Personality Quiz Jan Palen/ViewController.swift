ca9d9544eff9448b05a7a565b17fb8f39ffada6a
Jaanioo <jan.palen97@gmail.com>
2021-10-24 18:23:27 +0200

    Initial commit

---
+++ Personality Quiz Jan Palen/Personality Quiz Jan Palen/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Personality Quiz Jan Palen
+//
+//  Created by Student WSPA on 10.06.21.
+//  Copyright © 2021 Student WSPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindToQuizIntroduction( segue: UIStoryboardSegue) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
